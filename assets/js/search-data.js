4c2c4963510e130137b567f61bd8290c32f85881
Goulustis <43025723+Goulustis@users.noreply.github.com>
2025-02-17 12:09:25 +0000

    Deploying to gh-pages from @ Goulustis/goulustis.github.io@c3d324a1e82d6bb45c52be91126a5467f7a6f577 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -97,7 +97,7 @@ ninja.data = [{
           section: "Todos",},{id: "todos-cook-vegtables-swim-ticket-hotel-singapore-code-perspective-orthogonal-projections",
           title: 'cook vegtables   swim   ticket/hotel Singapore   code perspective/orthogonal projections',
           description: "",
-          section: "Todos",},{id: "todos-investigate-if-perspective-works-better-or-not-simple-excersize-look-at-singapore-and-nearby-to-decide-if-you-fly-back-immediately-after-conference",
+          section: "Todos",},{id: "todos-investigate-if-perspective-works-better-or-not-simple-excersize-look-at-singapore-and-nearby-to-decide-if-you-fly-back-immediately-after-conference-yeet-your-ass-just-yeet-your-ass-here-for-eye-thing",
           title: 'Investigate if perspective works better or not Simple excersize look at singapore and...',
           description: "",
           section: "Todos",},{
