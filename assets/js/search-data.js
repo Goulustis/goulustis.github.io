981a63152d037803761eb4230f7ce8d9637130b6
Goulustis <43025723+Goulustis@users.noreply.github.com>
2025-02-17 12:10:53 +0000

    Deploying to gh-pages from @ Goulustis/goulustis.github.io@fbb51074fecf8f110c7f15084c705c90a1ac1bd5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/todos/";
           },
         },{id: "nav-useful-links",
-          title: "useful links",
+          title: "Useful links",
           description: "",
           section: "Navigation",
           handler: () => {
